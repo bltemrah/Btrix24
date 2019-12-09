bb35fb517e29bc4398eb978fa74e7f9c823e3f81
Your Name <bltemrah@gmail.com>
2019-12-09 17:33:09 +0000

    Updating

--- src/test/java/com/Btrix24/tests/event.java
+++ src/test/java/com/Btrix24/tests/event.java
@@ -1,5 +1,6 @@
 package com.Btrix24.tests;
 
+import com.Btrix24.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
